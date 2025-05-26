adaf93697635dffd36b65870ca9cce23c2e9b572
Esteban Platero Horcajadas <plateroesteban@gmail.com>
2025-05-26 18:58:05 +0200

    fix: 🐛 removed duplicated jest config

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,12 +1,9 @@
 module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.js'],
+  testEnvironment: "node",
+  testMatch: ["**/tests/**/*.test.js"],
   collectCoverage: true,
-  coverageDirectory: 'coverage',
-  collectCoverageFrom: [
-    'src/**/*.js',
-    '!src/prisma/seed.js',
-    '!src/index.js'
-  ],
-  setupFilesAfterEnv: ['./tests/setup.js']
+  coverageDirectory: "coverage",
+  coverageReporters: ["json-summary", "text"],
+  collectCoverageFrom: ["src/**/*.js", "!src/prisma/seed.js", "!src/index.js"],
+  setupFilesAfterEnv: ["./tests/setup.js"],
 };
